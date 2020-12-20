e5f1f44c771b87edf335b179451df9c83dda14a2
Chris Reed <cr31293@gmail.com>
2020-12-19 21:45:14 -0500

    useEffect refactor

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -104,18 +104,56 @@ export default function Header(props) {
     ]
 
     useEffect(() => {
-        if (window.location.pathname === "/" && value !== 0) {
-            setValue(0);
-        } else if (window.location.pathname === "/services" && value !== 1) {
-            setValue(1);
-        } else if (window.location.pathname === "/revolution" && value !== 2) {
-            setValue(2);
-        } else if (window.location.pathname === "/about" && value !== 3) {
-            setValue(3);
-        } else if (window.location.pathname === "/contact" && value !== 4) {
-            setValue(4);
-        } else if (window.location.pathname === "/estimate" && value !== 5) {
-            setValue(5);
+        switch (window.location.pathname) {
+            case "/":
+                if (value !== 0) {
+                    setValue(0)
+                }
+                break;
+            case "/services":
+                if (value !== 1) {
+                setValue(1);
+                setSelectedIndex(1);
+                }
+                break;
+            case "/customsoftware":
+                if (value !== 1) {
+                    setValue(1);
+                    setSelectedIndex(1);
+                }
+                break;
+            case "/mobileapps":
+                if (value !== 1) {
+                    setValue(1);
+                    setSelectedIndex(2);
+                }
+                break;
+            case "/webistes":
+                if (value !== 1) {
+                    setValue(1);
+                    setSelectedIndex(3);
+                }
+                break;
+            case "/revolution":
+                if (value !== 2) {
+                    setValue(2);
+                }
+            case "/about":
+                if (value !== 3) {
+                    setValue(3);
+                }
+                break;
+            case "/contact":
+                if (value !== 4) {
+                    setValue(4);
+                }
+                break;
+            case "/estimate":
+                if (value !== 5) {
+                setValue(5);
+            }
+            default:
+                break;
         }
     }, [value]);
   return (
