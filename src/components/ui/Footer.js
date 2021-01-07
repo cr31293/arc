34fa175e1cbc149de6c4814536a7b65164471da1
Chris Reed <cr31293@gmail.com>
2021-01-06 21:29:09 -0500

    footer finished up for now

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,19 +1,32 @@
 import React from "react";
-import {makeStyles} from "@material-ui/styles";
-import footerAdornment from "../../assets/Footer Adornment.svg"
+import { makeStyles } from "@material-ui/styles";
+import footerAdornment from "../../assets/Footer Adornment.svg";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   footer: {
     backgroundColor: theme.palette.common.blue,
-    width: "100%"
+    width: "100%",
+    zIndex: 1302,
+    position: "relative"
+  },
+  adornment: {
+    width: "25em",
+    verticalAlign: "bottom",
+    [theme.breakpoints.down("md")]: {
+      width: "21em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "15em",
+    }
   }
 }));
 
 export default function () {
-  const classes = useStyles()
+  const classes = useStyles();
 
   return (
-    <footer className={classes.footer}><img alt="black decorative slash" src={footerAdornment} /></footer>
-
-  ) 
+    <footer className={classes.footer}>
+      <img className={classes.adornment} alt="black decorative slash" src={footerAdornment} />
+    </footer>
+  );
 }
