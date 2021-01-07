e2dccbf00b3a45a4e43480d732b43b0abfaa3d17
Chris Reed <cr31293@gmail.com>
2021-01-06 21:59:46 -0500

    added functional links to footer

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,5 +1,7 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import { makeStyles } from "@material-ui/styles";
+import Grid from "@material-ui/core/Grid";
 import footerAdornment from "../../assets/Footer Adornment.svg";
 
 const useStyles = makeStyles((theme) => ({
@@ -18,6 +20,20 @@ const useStyles = makeStyles((theme) => ({
     [theme.breakpoints.down("xs")]: {
       width: "15em",
     }
+  },
+  mainContainer: {
+    position: "absolute",
+
+  },
+  link: {
+    color: "white",
+    fontFamily: "Arial",
+    fontSize: "0.75rem",
+    fontWeight: "bold",
+    textDecoration: "none"
+  },
+  gridItem: {
+    margin: "3em"
   }
 }));
 
@@ -26,6 +42,67 @@ export default function () {
 
   return (
     <footer className={classes.footer}>
+      <Grid container className={classes.mainContainer} justify="center">
+        <Grid item className={classes.gridItem}>
+          <Grid container direction="column" spacing={2}>
+            <Grid item component={Link} to="/" className={classes.link}>
+              Home
+            </Grid>
+          </Grid>
+        </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid item component={Link} to="/services"  className={classes.link}>
+                Services
+              </Grid>
+              <Grid item component={Link} to="/customsoftware"  className={classes.link}>
+                Custom Software Development
+              </Grid>
+              <Grid item component={Link} to="/mobileapps"  className={classes.link}>
+                Mobile App Development
+              </Grid>
+              <Grid item component={Link} to="/websites"  className={classes.link}>
+                Website Development
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid item component={Link} to="/revolution" className={classes.link}>
+                The Revolution
+              </Grid>
+              <Grid item component={Link} to="/revolution" className={classes.link}>
+                Vision
+              </Grid>
+              <Grid item component={Link} to="/revolution" className={classes.link}>
+                Technology
+              </Grid>
+              <Grid item component={Link} to="/revolution" className={classes.link}>
+                Process
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid item component={Link} to="/about" className={classes.link}>
+                About Us
+              </Grid>
+              <Grid item component={Link} to="/about" className={classes.link}>
+                History
+              </Grid>
+              <Grid item component={Link} to="/about" className={classes.link}>
+                Team
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item className={classes.gridItem}>
+            <Grid container direction="column" spacing={2}>
+              <Grid item component={Link} to="/contact" className={classes.link}>
+                Contact Us
+              </Grid>
+            </Grid>
+          </Grid>
+      </Grid>
       <img className={classes.adornment} alt="black decorative slash" src={footerAdornment} />
     </footer>
   );
